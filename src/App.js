8e040b41596c44d7cae21f6a2f6513d4fcdb262a
MarlonVCS <marlonsouza.eu@gmail.com>
2022-06-06 04:03:04 -0300

    Novas Aulas do curso

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import './App.css';
-import SayMyName from './components/SayMyName';
-import Pessoa from './components/Pessoa';
-import Frase from './components/Frase';
-import List from './components/List';
-
+import Evento from './components/Evento'
+import Form from './components/Form';
 function App() {
-  const nome = "João"
   return (
     <div className="App">
-      <h1>Testando css</h1>
-      <Frase />
-      <Frase />
-      <SayMyName nome="Marlon"/>
-      <SayMyName nome="Maria"/>
-      <SayMyName nome={nome}/>
-      <Pessoa
-        nome="Rodrigo"
-        idade="28"
-        profissão="Desenvolvedor"
-        foto="https://via.placeholder.com/150"
-      />
-      <List />
+      <h1>Testando Eventos</h1>
+      <Evento numero="1" />
+      <Evento numero="2" />
+      <Form />
     </div>
   );
 }
--- src/components/List.js
+++ src/components/List.js
@@ -1,16 +1,15 @@
 import Item from "./item"
-
 function List() {
     return(
         <>
             <h1>Minha Lista</h1>
             <ul>
-                <Item marca="ferrari"/>
-                <Item marca="Renault"/>
-                <Item marca="Fiat"/>
+                <Item marca="ferrari" ano_lancamento={1980}      />
+                <Item marca="Renault" ano_lancamento={1964}      />
+                <Item marca=   "Fiat" ano_lancamento={1978}      />
+                <Item                                            />
             </ul>
         </>
     )
 }
-
 export default List
